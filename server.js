9eab68c512a769c9e8bb90d3cf4d1884d17b57b6
saikham <saikham.email@gmail.com>
2017-12-19 12:07:48 +0630

    Deploying to Heroku server- Testing

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.originalUrl}`;
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if(err){
+			console.log('Error occurs.');
+		}
+	});
+	next();
+});
+
+app.use((req, res, next) => {
+	res.render('maintenance.hbs');
+});
+
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+app.get('/',(req, res) => {
+	res.render('home.hbs',{
+		pageTitle: 'Home Page',
+		welcomeText: 'Welcome User'
+	})
+});
+
+app.get('/about',(req, res) => {
+	res.render('about.hbs',{
+		pageTitle: 'About Page'
+	});
+});
+
+app.get('/bad',(req, res) => {
+	res.send({
+		error: 'Not Found.'
+	});
+});
+
+app.listen(port, () => {
+	console.log('Server is up on port 3000.');
+});
